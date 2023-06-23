c9c3b68890315989dc77ed2557063972e525944e
LatteDiMandorla <giovannidg02@gmail.com>
2023-06-23 11:55:56 +0200

    Prova main2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 import java.sql.*;
 public class Main {
     public static void main(String[] args) throws SQLException {
+        Connection con = Database.getConnection();
 
+        if (con!=null) {
+            System.out.println("Successo!");
+        }
+        else {
+            System.out.println("No!");
+        }
+    }
 }
