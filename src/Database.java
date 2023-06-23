169818cf6faa17cc746a6d698acd3d40a2e10853
Raffaele Avallone <raffaele.avallone02@gmail.com>
2023-06-23 11:05:20 +0200

    DB

---
+++ src/Database.java
@@ -0,0 +1,15 @@
+import java.sql.*;
+public class Database {
+    private static String url="jdbc:postgresql://localhost:5432/CENTRO DI RICOVERO PER TARTARUGHE MARINE";
+    private static String user="postgres";
+    private static String password="password";
+
+    private Database() {}
+
+    public static Connection getConnection() throws SQLException {
+        Connection connection = null;
+        connection = DriverManager.getConnection(url, user, password);
+
+        return connection;
+    }
+}
