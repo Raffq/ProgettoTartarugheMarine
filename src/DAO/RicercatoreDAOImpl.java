98383ce507dd2a8544382a3cdf0ef71a8b764356
Raffaele Avallone <raffaele.avallone02@gmail.com>
2023-06-29 17:53:54 +0200

    Implementazione classe RicercatoreDAOImpl

---
+++ src/DAO/RicercatoreDAOImpl.java
@@ -0,0 +1,37 @@
+package DAO;
+
+import ClassiPrincipali.Medico_Veterinario;
+import ClassiPrincipali.Ricercatore;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class RicercatoreDAOImpl implements RicercatoreDAO{
+    @Override
+    public Ricercatore get(String id) throws SQLException {
+        Connection con = Database.getConnection();
+        Ricercatore ricercatore = null;
+
+        String sql = "Select * From ricercatore Where matricolarc = ?"; //TODO: cambiare nel database il nome da matricolaric a matricolarc
+        PreparedStatement ps = con.prepareStatement(sql);
+
+        ps.setString(1, id);
+        ResultSet resultSet = ps.executeQuery();
+        try {
+            if (resultSet.next()) {
+                String matricola = resultSet.getString("matricolaric");
+                String nome = resultSet.getString("nome");
+                String cognome = resultSet.getString("cognome");
+                String fkidcentro = resultSet.getString("fkidcentro");
+
+                ricercatore = new Ricercatore(matricola, nome, cognome, fkidcentro);
+            }
+        } catch (SQLException e) {
+            System.out.println("Qualcosa è andato storto!");
+        }
+        return ricercatore;
+    }
+    }
+}
---
+++ src/DAO/Tecnico_LaboratorioDAOImpl.java
@@ -0,0 +1,12 @@
+package DAO;
+
+import ClassiPrincipali.Tecnico_Laboratorio;
+
+import java.sql.SQLException;
+
+public class Tecnico_LaboratorioDAOImpl implements Tecnico_LaboratorioDAO{
+    @Override
+    public Tecnico_Laboratorio get(String id) throws SQLException {
+        return null;
+    }
+}
