bbd8ba394685b753729a6339bd7884197175d794
Raffaele Avallone <raffaele.avallone02@gmail.com>
2023-06-30 11:30:59 +0200

    Piccolo fix

--- src/DAO/Medico_VeterinarioDAO.java
+++ src/DAO/Medico_VeterinarioDAO.java
@@ -9,5 +9,5 @@ public interface Medico_VeterinarioDAO extends DAO<Medico_Veterinario>{
     public void compileLastCartellaClinica(String idTartaruga, int lunghezza, int larghezza, int peso, String luogo_ritrovamento, String matricola) throws SQLException;
     public void compileComponenti(String idTartaruga, Date dataCompilazione, String descBecco, String descCollo, String descTesta, String descCoda, String descPinne, String descOcchi, String descNaso) throws SQLException;
     public void updateCartellaClinica(String idCartellaClinica, int lunghezza, int larghezza, int peso, String luogo_ritrovamento, String matricola) throws SQLException;
-    public void updateComponenti(String idComponenti, Date dataCompilazione, String descBecco, String descCollo, String descTesta, String descCoda, String descPinne, String descOcchi, String descNaso) throws SQLException
+    public void updateComponenti(String idComponenti, Date dataCompilazione, String descBecco, String descCollo, String descTesta, String descCoda, String descPinne, String descOcchi, String descNaso) throws SQLException;
 }
